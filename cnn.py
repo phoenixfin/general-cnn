2cce185e596ada7a2a3d462fd8402cabf1a0900a
Aditya Firman Ihsan <aditya.fphoenix@gmail.com>
2020-06-05 07:40:17 +0700

    add some complexity

--- cnn.py
+++ cnn.py
@@ -16,11 +16,12 @@ class BaseConvolutionalNetwork(object):
     
     # Default Hyperparameter
     hyperparam = {
-        'batch_size'    : 20, 
-        'epoch'         : 100,
+        'batch_size'    : 32, 
+        'epochs'        : 50,
+        'learning_rate' : 0.001,
+        'input_size'    : 300,    
         'steps'         : 100,
-        'val_steps'     : 50,
-        'learning_rate' : 0.001            
+        'val_steps'     : 50
     }
     
     # Default Augmentation
@@ -49,15 +50,14 @@ class BaseConvolutionalNetwork(object):
         'dtype'                           : None        
     }
     
-    def __init__(self, name_dir='', input_=(), load=False, mode='binary'):
+    def __init__(self, name_dir, save_file='', load=False):
         if not load:
             self.create_new()
         else:
             self.load(load)
-        self.input_shape = input_
-        self.mode = mode
         self.name = name_dir
         self.obtain_image_data()
+        self.save = save_file if save_file else name_dir
             
     def show_summary(self):
         self.model.summary()
@@ -93,25 +93,38 @@ class BaseConvolutionalNetwork(object):
     
     def add_convolution(self, filter_num, kernels_size, 
                         pooling = (2,2), activation = 'relu', 
-                        dropout = 0, first = True):
+                        dropout = 0, normalize = False, first = True):
         kwargs = {'activation':activation}
+        in_ = self.hyperparam['input_size']
         for i, num in enumerate(filter_num):
             kwargs['filters'] = num
             kwargs['kernel_size'] = kernels_size[i]
-            if first and i==0: kwargs['input_shape']=self.input_shape + (3,)
+            if first and i==0: 
+                kwargs['input_shape']= (in_, in_, 3)
+
             self.model.add(tf.keras.layers.Conv2D(**kwargs))
+
             if pooling: 
                 self.model.add(tf.keras.layers.MaxPooling2D(*pooling))
+            if normalize:
+                self.model.add(tf.keras.layers.BatchNormalization())
+
         if dropout:
             self.model.add(tf.keras.layers.Dropout(dropout))
+
         self.model.add(tf.keras.layers.Flatten())
         
-    def set_hidden_layers(self, neurons_list, activation='relu'):
+    def set_hidden_layers(self, neurons_list, dropout = 0, 
+                          normalize = False, activation='relu'):
         for num in neurons_list:
-            hidden = tf.keras.layers.Dense(num, activation=activation)
+            hidden = tf.keras.layers.Dense(num, activation=activation)           
             self.model.add(hidden)
-
-    def set_output_layer(self, activation='sigmoid'):
+            if dropout:
+                self.model.add(tf.keras.layers.Dropout(dropout))
+            if normalize:
+                self.model.add(tf.keras.layers.BatchNormalization())
+                
+    def set_output_layer(self):
         if self.mode == 'binary':
             activation = 'sigmoid'
         elif self.mode == 'categorical':
@@ -136,21 +149,26 @@ class BaseConvolutionalNetwork(object):
         val_dir = base_dir+'\\tobeused\\validation'
                 
         IMG = tf.keras.preprocessing.image.ImageDataGenerator
+        
         # tg = IMG(**self.augmentation)
-        tg = IMG(rescale = 1.0/255.)
-        vg = IMG(rescale = 1.0/255.)
+        tg = IMG(rescale = 1./255)
+        vg = IMG(rescale = 1./255)
+
+        in_ = self.hyperparam['input_size']
+        classes = len(os.listdir(train_dir))
+        self.mode = 'binary' if classes == 2 else 'categorical'
 
         train_data = tg.flow_from_directory(
             train_dir,
             batch_size = self.hyperparam['batch_size'],
             class_mode = self.mode,
-            target_size = self.input_shape
+            target_size = (in_, in_)
         )
         validation_data =  vg.flow_from_directory(
             val_dir,
             batch_size = self.hyperparam['batch_size'],
             class_mode = self.mode,
-            target_size = self.input_shape
+            target_size = (in_, in_)
         )
         self.image_data = {'train':train_data, 'val':validation_data}
 
@@ -160,7 +178,7 @@ class BaseConvolutionalNetwork(object):
 
         # no progress stopping callback
         if 'no_progress_stopping' in cb_list:
-            impatient = CB.EarlyStopping(monitor='accuracy', patience=3)
+            impatient = CB.EarlyStopping(monitor='val_accuracy', patience=3)
             callbacks.append(impatient)
 
         # save per epoch callback
@@ -185,10 +203,9 @@ class BaseConvolutionalNetwork(object):
         return callbacks
 
     def train(self, save=False):
-        p = self.hyperparam
-        
         # Compile the model
-        opt = tf.keras.optimizers.RMSprop(lr=p['learning_rate'])
+        lr = self.hyperparam['learning_rate']
+        opt = tf.keras.optimizers.Adam(learning_rate=lr)
         loss = self.mode + '_crossentropy'
         self.model.compile(optimizer=opt, loss=loss, metrics=['accuracy'])
 
@@ -202,10 +219,9 @@ class BaseConvolutionalNetwork(object):
         history = self.model.fit(
             self.image_data['train'],
             validation_data = self.image_data['val'],
-            steps_per_epoch = p['steps'],
-            epochs = p['epoch'],
-            validation_steps = p['val_steps'],
-            verbose = 2,
+            steps_per_epoch = self.hyperparam['steps'],
+            epochs = self.hyperparam['epochs'],
+            validation_steps = self.hyperparam['val_steps'],
             callbacks = callbacks
         )
         return history
@@ -214,19 +230,17 @@ class BaseConvolutionalNetwork(object):
         IM = tf.keras.preprocessing.image
 
         data_dir = os.path.join(os.getcwd(), 'data', self.name)
-        category = os.listdir(data_dir+'\\tobeused\\train')
+        category = list(self.image_data['train'].class_indices.keys())
         path = os.path.join(data_dir+'\\test') 
-        test_data = os.listdir(path)
-        randomized = random.sample(test_data, num)
-
-        for file in randomized:
-            img = IM.load_img(path+'\\'+file, target_size = self.input_shape)
-
-            x = IM.img_to_array(img)
-            x = np.expand_dims(x, axis=0)
-            images = np.vstack([x])
-            res = self.model.predict(images, batch_size=10)
-
+        test_data = random.sample(os.listdir(path), num)
+        size = self.hyperparam['input_size']
+        
+        for file in test_data:
+            img = IM.load_img(path+'\\'+file, target_size = (size, size))
+            img_array = IM.img_to_array(img_array)
+            normalized = np.expand_dims(img, axis=0)/255
+            
+            res = self.model.predict(normalized, batch_size=10)
             print(file, ': ',category[np.argmax(res[0])])
 
     def report(self, history):
